259f0ef90a9fe4bea1218ae9fef9cb7bed1f963f
Ezhil E <ezhilengels@gmail.com>
2020-09-03 08:37:46 +0530

    some cgahhe

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
-const express = require('express');
-const path = require('path')
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+
+import App from "../src/App";
+
+const PORT = 8000;
 
 const app = express();
-const publicpath = path.join(__dirname, '..', 'public');
 
-const port = process.env.PORT || 3000;
+app.use("^/$", (req, res, next) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    );
+  });
+});
 
-app.use(express.static(publicpath));
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
-app.get('*', (req, res) =>{
-    res.sendFile(path.join(publicpath, 'index.html'))
-})
-
-app.listen(port, () => {
-    console.log('Server is running on port ' + port)
-})
+app.listen(PORT, () => {
+  console.log(`App launched on ${PORT}`);
+});
